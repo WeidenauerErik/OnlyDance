5c04f3ba759df59b49b99e8e482b79e36a42b34a
Theodosiusss <1202@htl.rennweg.at>
2025-05-13 13:11:23 +0200

    new Fixtures and new add functionality

--- symfony-docker/src/Controller/StepsequenceStepController.php
+++ symfony-docker/src/Controller/StepsequenceStepController.php
@@ -48,10 +48,54 @@ class StepsequenceStepController extends AbstractController
 
         $file = fopen($dir.str_replace(' ', '', $data['name']).".json","w");
         fwrite($file,$request->getContent());
-
         fclose($file);
 
-        return new JsonResponse(["status" => "successfully created file"]);
+        $stepsequence = new Stepsequence();
+
+        $stepsequence->setDance($dance);
+        $stepsequence->setBadge($badge);
+        $stepsequence->setName($data["name"]);
+        $stepsequence->setDifficulty($data["difficulty"]);
+
+
+        foreach ($data['steps'] as $stepData) {
+            $step = new Step();
+
+            $step->setHowquick($stepData['howquick']);
+
+            $step->setM1X($stepData['m1_x']);
+            $step->setM1Y($stepData['m1_y']);
+            $step->setM1Rotate($stepData['m1_rotate']);
+            $step->setM1Heel($stepData['m1_heel']);
+            $step->setM1Toe($stepData['m1_toe']);
+
+            $step->setM2X($stepData['m2_x']);
+            $step->setM2Y($stepData['m2_y']);
+            $step->setM2Rotate($stepData['m2_rotate']);
+            $step->setM2Heel($stepData['m2_heel']);
+            $step->setM2Toe($stepData['m2_toe']);
+
+            $step->setW1X($stepData['w1_x']);
+            $step->setW1Y($stepData['w1_y']);
+            $step->setW1Rotate($stepData['w1_rotate']);
+            $step->setW1Heel($stepData['w1_heel']);
+            $step->setW1Toe($stepData['w1_toe']);
+
+            $step->setW2X($stepData['w2_x']);
+            $step->setW2Y($stepData['w2_y']);
+            $step->setW2Rotate($stepData['w2_rotate']);
+            $step->setW2Heel($stepData['w2_heel']);
+            $step->setW2Toe($stepData['w2_toe']);
+
+            $stepsequence->addHowquick($step);
+
+            $entityManager->persist($step);
+
+        }
+
+        $entityManager->persist($stepsequence);
+        $entityManager->flush();
+        return new JsonResponse(['status' => 'Dance and steps saved and successfully created file'], 201);
     }
     #[Route('/stepsequence/add', name: 'app_stepsequence_new', methods: ['POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
